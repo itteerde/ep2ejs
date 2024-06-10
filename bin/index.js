94dcba1b7fe948761d7b2c96c6ee209d5716b629
Erik Itter <erik.itter@gmail.com>
2024-06-10 23:48:54 +0200

    design

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
-
+import { Level } from "level";
 
 
 program
@@ -9,7 +9,21 @@ program
     .description("Eclipse Phase 2nd Edition Tools")
     .option("-f, --file <path>", "data.json")
     .action((options) => {
-        console.log(`Hey, ${options.file}!`);
+        //console.log(`Hey, ${options.file}!`);
     });
 
 program.parse(process.argv);
+
+const db = new Level('./items', { valueEncoding: 'json' });
+//await db.put('a', 'Hello World!');
+
+//console.log(await db.get('a'));
+
+/*
+for await (const [key, value] of db.iterator()) { // { gt: 'a' }
+    console.log({ key: key, value: value });
+}
+    */
+
+const entries = await db.iterator().all();
+console.log(entries.length)
